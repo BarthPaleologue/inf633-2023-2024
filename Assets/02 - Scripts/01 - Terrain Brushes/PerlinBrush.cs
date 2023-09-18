36259733655f89cfe7e90f391954137018bf9be0
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2023-09-18 16:10:08 +0200

    Added perlin brush

---
+++ Assets/02 - Scripts/01 - Terrain Brushes/PerlinBrush.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerlinBrush : TerrainBrush
+{
+    public bool isIncreasing = true;
+    public float increment = 1;
+    public float noiseFrequency = 0.1f;
+    public int nbOctaves = 1;
+    public override void draw(int x, int z)
+    {
+        for (int zi = -radius; zi <= radius; zi++)
+        {
+            for (int xi = -radius; xi <= radius; xi++)
+            {
+                float previousHeight = terrain.get(x + xi, z + zi);
+                float perlin = 0.0f;
+                float heightAcc = 0.0f;
+                for(int i = 0; i < nbOctaves; i++)
+                {
+                    perlin += Mathf.PerlinNoise((x + xi) * noiseFrequency * (i + 1), (z + zi) * noiseFrequency * (i + 1)) / Mathf.Pow(2.0f, (float)i);
+                    heightAcc += 1.0f / Mathf.Pow(2.0f, (float)i);
+                }
+                perlin /= heightAcc;
+
+                float newHeight = isIncreasing ? previousHeight + increment * perlin : previousHeight - increment * perlin;
+                terrain.set(x + xi, z + zi, newHeight);
+            }
+        }
+    }
+}
