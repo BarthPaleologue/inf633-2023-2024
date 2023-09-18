290905fe1e00b9d1f8d37f81aac996b5386d193c
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2023-09-18 15:08:22 +0200

    Added FlatBrush

--- Assets/02 - Scripts/01 - Terrain Brushes/FlatBrush.cs
+++ Assets/02 - Scripts/01 - Terrain Brushes/FlatBrush.cs
@@ -1,19 +1,16 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SimpleBrush : TerrainBrush
+public class FlatBrush : TerrainBrush
 {
-
-    public float height = 5;
-
     public override void draw(int x, int z)
     {
         for (int zi = -radius; zi <= radius; zi++)
         {
             for (int xi = -radius; xi <= radius; xi++)
             {
-                terrain.set(x + xi, z + zi, height);
+                terrain.set(x + xi, z + zi, 0);
             }
         }
     }
