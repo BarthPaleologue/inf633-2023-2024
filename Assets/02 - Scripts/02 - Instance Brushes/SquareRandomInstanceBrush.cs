06cfff289b1bb4c384e54e916fd8f5117dbeb154
Ghislain Kengne Gumete <kengneghislain1@gmail.com>
2023-09-25 16:08:02 +0200

    min distance instance brush

--- Assets/02 - Scripts/02 - Instance Brushes/SquareRandomInstanceBrush.cs
+++ Assets/02 - Scripts/02 - Instance Brushes/SquareRandomInstanceBrush.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class SquareRandomInstanceBrush : InstanceBrush {
 
+    public int count = 5;
+
     public override void draw(float x, float z) {
-      float xDisplace = (Random.value-0.5f)*radius;
-      float zDisplace = (Random.value-0.5f)*radius;
-      spawnObject(x+xDisplace, z+zDisplace);
+      for(int i=0; i<count; i++) {
+        float xDisplace = (Random.value-0.5f)*radius;
+        float zDisplace = (Random.value-0.5f)*radius;
+        spawnObject(x+xDisplace, z+zDisplace);
+      }
     }
 }
